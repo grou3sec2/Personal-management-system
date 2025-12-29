21197bc17f58f6efa300eb0c2d1c305f49710fc3
Jeremiah Tesfaye <jeremiahtesfaye3@gmail.com>
2025-12-29 10:05:53 +0300

    updating database connection file called DBConnection.java

--- src/main/java/com/pfms/util/DBConnection.java
+++ src/main/java/com/pfms/util/DBConnection.java
@@ -1,16 +1,35 @@
 package com.pfms.util;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class DBConnection {
-    // Member 1(Jeremiah): 
-    private static final String URL = "jdbc:mysql://localhost:3306/personal_finance_db";
+
+    // Update 'personal_finance_db' with your actual MySQL database name
+    private static final String URL = "jdbc:mysql://localhost:3306/personal_finance_db?useSSL=false&allowPublicKeyRetrieval=true";
     private static final String USER = "root";
-    private static final String PASSWORD = "YOUR_LOCAL_PASSWORD_HERE"; //use your local database and password
+    private static final String PASSWORD = "1234"; 
 
     public static Connection getConnection() {
-        
-        return null; 
+        Connection conn = null;
+        try {
+            // Modern MySQL driver class name
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("connected successfully");
+           
+            
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return conn;
+    }
+    
+    public static void main(String[] args){
+        Connection c = getConnection();
     }
 }
+
